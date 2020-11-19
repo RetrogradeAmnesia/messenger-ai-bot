bb341804e6c8f833b3555d1ef3583641d9492f12
Retrograde Amnesia <31753563+RetrogradeAmnesia@users.noreply.github.com>
2020-11-19 18:16:12 +1100

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const client = new Wit({
   accessToken: process.env.WIT_TOKEN,
   logger: new log.Logger(log.DEBUG) // optional
 });
+console.log(client.message('set an alarm tomorrow at 7am'));
 
 // Webserver parameter
 const PORT = process.env.PORT || 8445;
